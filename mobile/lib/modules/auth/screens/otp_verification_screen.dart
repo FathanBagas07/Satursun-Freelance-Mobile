b8dba530a2dbbf86977d151ca6f3de02c1120c04
FathanBagas07 <fatan250217@gmail.com>
2025-11-20 09:51:47 +0700

    Fix: add ignore (library_private_types_in_public_api)

--- mobile/lib/modules/auth/screens/otp_verification_screen.dart
+++ mobile/lib/modules/auth/screens/otp_verification_screen.dart
@@ -9,6 +9,7 @@ class OtpVerificationScreen extends StatefulWidget {
   const OtpVerificationScreen({super.key, required this.contactInfo});
 
   @override
+  // ignore: library_private_types_in_public_api
   _OtpVerificationScreenState createState() => _OtpVerificationScreenState();
 }
 
