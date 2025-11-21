68a18055332661e301e1374211ce3ed9ccbfd791
FathanBagas07 <fatan250217@gmail.com>
2025-11-21 09:01:38 +0700

    Feat: add color variabels global (app_color)

---
+++ mobile/lib/core/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // PRIMARY & BRAND COLORS
+  static const Color saturSunBlue = Color(0xFF1E88E5);
+  static const Color saturSunOrange = Color(0xFFF98B00);
+  static const Color saturSunLightBlue = Color(0xFF8AC1F5); // Digunakan untuk BG/Gradient
+
+  // STATUS & ACCENT COLORS
+  static const Color saturSunRed = Color(0xFFE53935);
+  static const Color saturSunGreen = Color(0xFF4CAF50);
+  static const Color saturSunYellow = Color(0xFFFFC107);
+  static const Color saturSunGrey = Color(0xFFBDBDBD); // Ikon/Border Sekunder
+
+  // NEUTRAL COLORS
+  static const Color textDark = Color(0xFF212121);
+  static const Color textLight = Color(0xFF757575);
+  static const Color backgroundWhite = Colors.white;
+}
