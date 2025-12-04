abd1e4101160dab1c35d527b53d8bb6ccedb9e01
FathanBagas07 <fatan250217@gmail.com>
2025-12-04 09:54:29 +0700

    Refactor: update user table

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['freelancer', 'client']);
+            $table->string("photo")->nullable();
+            $table->text('bio')->nullable();
+            $table->string('firebase_uid')->unique();
             $table->timestamps();
         });
     }
