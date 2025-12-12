1d91385b8573fccf0205e800739467abb55c97c9
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2025-12-12 16:05:36 +0700

    Fix: delete initialize

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'core/theme/app_theme.dart';
 import 'core/routes/app_router.dart';
@@ -8,7 +7,6 @@ import 'core/routes/app_router.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await GoogleSignIn.instance.initialize();
 
   runApp(const SatursunApp());
 }
