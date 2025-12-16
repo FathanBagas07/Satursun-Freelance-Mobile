e64ea65e7badd447734b764a3df388bee252e9c7
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2025-12-16 21:34:29 +0700

    Refactor: remove unnecessary code

--- mobile/lib/core/services/auth_service.dart
+++ mobile/lib/core/services/auth_service.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:satursun_app/core/services/user_service.dart';
-
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
@@ -60,13 +58,6 @@ class AuthService {
         message: 'Login dibatalkan',
       );
     }
-
-    // Memastikan user ada di Firestore
-    await userService.createUserIfNotExists(
-      firstName: user.displayName?.split(' ').first ?? '',
-      lastName: user.displayName?.split(' ').skip(1).join(' ') ?? '',
-      email: user.email ?? '', uid: '', username: '',
-    );
 
     await FirebaseAuth.instance.signOut();
   }
