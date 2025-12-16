f496f0576452a8acfdea0ba643f88b7e5a481d87
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2025-12-16 10:03:41 +0700

    Feat: add route middleware

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/auth/register', [AuthController::class, 'register'])
+    ->middleware('firebase.auth');
