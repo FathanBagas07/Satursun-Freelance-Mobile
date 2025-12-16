92240e24c6001b8e8fe5cf4fc87977f946e03e00
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2025-12-17 01:14:27 +0700

    Feat: add function createUserIfNotExists in signInWithGoogle

--- mobile/lib/core/services/auth_service.dart
+++ mobile/lib/core/services/auth_service.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:satursun_app/core/services/user_service.dart';
+
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
@@ -31,22 +33,33 @@ class AuthService {
   }
 
   Future<User?> signInWithGoogle() async {
-  final GoogleSignInAccount? googleUser =
-      await GoogleSignIn().signIn();
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser == null) return null;
 
-  if (googleUser == null) return null;
+    final googleAuth = await googleUser.authentication;
 
-  final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
 
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth.accessToken,
-    idToken: googleAuth.idToken,
-  );
+    final userCredential = await FirebaseAuth.instance.signInWithCredential(
+      credential,
+    );
+    final user = userCredential.user;
 
-  final userCredential =
-      await FirebaseAuth.instance.signInWithCredential(credential);
+    if (user != null) {
+      // Buat user di Firestore jika belum ada
+      await userService.createUserIfNotExists(
+        firstName: user.displayName ?? user.email!.split('@')[0],
+        lastName: '',
+        email: user.email ?? '',
+        uid: user.uid,
+        username: user.displayName ?? user.email!.split('@')[0],
+      );
+    }
 
-  return userCredential.user;
+    return user;
   }
 
   Future<void> signOut() async {
