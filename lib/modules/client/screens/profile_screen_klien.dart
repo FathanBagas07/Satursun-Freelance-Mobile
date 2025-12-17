07ed17dc5bebc06d81ead766fe8db15b084c7043
FathanBagas07 <145728133+FathanBagas07@users.noreply.github.com>
2025-12-18 00:23:03 +0700

    Refactor: rename import

--- lib/modules/client/screens/profile_screen_klien.dart
+++ lib/modules/client/screens/profile_screen_klien.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:satursun_app/modules/auth/services/auth_service.dart';
-import '../../../core/widgets/custom_bottom_nav_bar_klien.dart';
+import 'package:satursun_app/core/widgets/custom_bottom_nav_bar_klien.dart';
 
 class ProfileKlienScreen extends StatefulWidget {
   const ProfileKlienScreen({super.key});
